559b804f2106db09f0aa51ecf586d83bed4bc92d
madtab7 <madtab7@gmail.com>
2018-10-22 17:30:39 -0400

    some updates

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
     if !!@user && !!@user.authenticate(params[:password])
-      render json: @user.id
+      render json: {id: @user.id, username: @user.username, comments: @user.comments, valid: true}
     else
-      render json: { errors: "Incorrect Username or Password" }, status: :unprocessible_entity
+      render json: { errors: "Incorrect Username or Password",  valid: false }, status: :unprocessible_entity
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Location.destroy_all
 
 
 user1 = User.create(username: "madtab", name:"Maddy", email:"mt@aol.com", password:"maddy")
+
+locations = Location.create([
+  {
+  "user_id": 1,
+  "latitude": "40.7062219633417",
+  "longitude": "-74.01455500571569",
+  "title": "flatiron school",
+  "description": "too hot "
+},
+{
+  "user_id": 1,
+  "latitude": "40.714285858313985",
+  "longitude": "-74.00837013428384",
+  "title": "chambers st. subway",
+  "description": "hate this subway station"
+}
+
+])
