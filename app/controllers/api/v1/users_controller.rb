43c7c2de1a7348245aba83eaa495054271badc4a
github username <github email address>
2018-10-23 17:31:12 -0400

    fixed validation errors for signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,7 +17,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user =  User.new(user_params)
     if @user.save
-      render json: @user
+      render json: {id: @user.id, username: @user.username, comments: @user.comments, valid: true}
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
     end
